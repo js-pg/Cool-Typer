e3b8501c77f22305269de5c90b7cd113f2480ad8
BigMuscleMann <zanekg123@gmail.com>
2021-09-16 17:40:40 -0500

    worked on func

--- js/styleChange.js
+++ js/styleChange.js
@@ -1,9 +1,20 @@
 const navDesktop = document.getElementById('navDesktop')
+const mainLi = document.getElementById('mainLi')
+li1 = document.getElementById('li1')
+li2 = document.getElementById('li2')
+li3 = document.getElementById('li3')
+
+
+
 
 function paletteChange(targetStyle) {
     if (targetStyle == 'default') {
-        navDesktop.style.backgroundColor = 'white'
-    }
-}
+        navDesktop.style.backgroundColor = 'white';
+        mainLi.style.color = 'black'
+        li1.style.color = '#585858'
+        li2.style.color = '#585858'
+        li3.style.color = '#585858'
+    };
+};
 
 paletteChange('default');
