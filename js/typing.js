a9e67d71f92753caf5375ce9929ec268add5631e
BigMuscleMann <zanekg123@gmail.com>
2021-09-19 19:59:29 -0500

    add quote file

--- js/typing.js
+++ js/typing.js
@@ -1,23 +1,12 @@
- function getQuote() {
- 
- fetch('https://api.quotable.io/random')
-     .then(response => response.json())
-     .then(data => document.getElementById('mainPlaceholder').innerHTML = data.content );
- }
- 
- getQuote()
-// 
-// window.onkeydown = function(e) {
-//     if(e.keyCode == 32 && e.target == document.getElementById('main')) {
-//         e.preventDefault();
-//         return false;
-//     }
-// };
-// 
-// window.onkeypress = function(event) {
-//     console.log(String.fromCharCode(event.keyCode))
-//     document.getElementById('mainInput').innerHTML += (String.fromCharCode(event.keyCode))
-//     document.getElementById('about').style.display = "none";
-//     scrollMain()
-// }
-// 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+
+
+nthQuote = getRandomInt(1594)
+
+const every_nth = (arr, nth) => arr.filter((e, i) => i % nth === nth - 1);
+console.log(every_nth(quotes, 1));
+
+
